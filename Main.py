ebd240d493b85fe8d75424144231376e54f6e070
PierreAubailly-bit <pierre1aubailly@gmail.com>
2024-12-08 17:36:36 +0100

    rien de fou

--- Main.py
+++ Main.py
@@ -3,11 +3,6 @@ from Game import Game
 from Player import Player
 from Alien import Alien
 from AlienFleet import AlienFleet
-import random
-from tkinter import *
-import os
-from PIL import Image, ImageTk
-
 
 if __name__ == "__main__":
     jeu = Accueil()
